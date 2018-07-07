5a3e83e2353b03bf3566a963b48907d95227af31
acduroy <36899232+acduroy@users.noreply.github.com>
2018-07-07 16:46:49 -0700

    Update maas-add-vm.sh

--- maas-add-vm.sh
+++ maas-add-vm.sh
@@ -17,18 +17,19 @@ printf "Use the IP address of the KVM Host bridge (ex. br201 - 10.100.201.2)\n"
 # For this example, user_name = User Name of KVM Host (ex. 'acd')
 # and IP address of the kvm host bridge (ex. br201 - 10.100.201.2)
 # Example ssh-copy-id -i ~/..sh/id_rsa acd@10.100.201.2
-
+printf "\n"
 printf "Example ssh-copy-id -i ~/.ssh/id_rsa acd@10.100.201.2\n"
 read -p "Press any key, once ready to enter data ..."
-
+printf "\n"
 echo -n "Enter your kvm host user name: "; read KVM_USER
 echo -n "Enter your kvm ip address: "; read KVM_HOST
 ssh-copy-id -i ~/.ssh/id_rsa $KVM_USER@$KVM_HOST
 
+clear
 printf "Testing the connection between MAAS and KVM-Host ...\n"
 virsh -c qemu+ssh://$KVM_USER@$KVM_HOST/system list --all
 printf "Once connection has been checked, you can now exit MAAS shell\n"
-echo -n "Press 'Y' to exit, 'n' to stay at MAAS shell"; read EXIT_SHELL
+echo -n "Press 'Y' to exit, 'n' to stay at MAAS shell: "; read EXIT_SHELL
 EXIT_SHELL=$(echo $EXIT_SHELL | awk '{print toupper($0)}')
 if [[ EXIT_SHELL == 'Y' ]]; then
    exit 
