6c55c735236b9b1340c4706afb414a6512b19238
acduroy <36899232+acduroy@users.noreply.github.com>
2019-02-07 10:47:12 -0800

    Update bridge-net.sh

--- bridge-net.sh
+++ bridge-net.sh
@@ -38,7 +38,7 @@ brctl addif $BNAME $INT
 sudo cp /etc/network/interfaces /etc/network/interfaces.bak
 cat << BRIDGE | sudo tee /etc/network/interfaces
 
-source /etc/network/interfaces.d/* <---- this should not be in desktop version??? (it's better to just append the original file)
+source /etc/network/interfaces.d/* # <---- this should not be in desktop version??? (it's better to just append the original file)
 # The loopback network interface
 auto lo
 iface lo inet loopback
