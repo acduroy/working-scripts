d550671e35ff9f0a425a42484dc3db0e17ad0dd8
acduroy <36899232+acduroy@users.noreply.github.com>
2020-04-02 10:09:11 -0700

    Update nat-script-vm.sh

--- nat-script-vm.sh
+++ nat-script-vm.sh
@@ -4,7 +4,7 @@
 #Description: This script will enable nat on a virtual machine with three (3) nic interfaces
 #Usage: ./nat-script-vm.sh
 
-install packages
+#install packages
 sudo apt-get install -y iptables-persistent
 
 #Show nic interfaces
@@ -15,12 +15,12 @@ echo -n "Hostname (e.g. gw0): "; read HNAME
 echo -n "Internal network interface (e.g. enp0s3): "; read INT
 echo -n "Internal network interface address (e.g. 10.0.0.1): "; read INT_ADDR
 echo -n "External network interface (e.g. enp0s8): "; read EXT
-echo -n "Host-only network interface (e.g. enp0s9): "; read HOSTONLY
+#echo -n "Host-only network interface (e.g. enp0s9): "; read HOSTONLY
 
 # Change host name
-sudo hostnamectl set-hostname $HNAME
-sudo cp /etc/hosts /etc/hosts.backup
-sudo sed -i -e "s/^127.0.1.1.*$/127.0.1.1\t$HNAME/" /etc/hosts
+#sudo hostnamectl set-hostname $HNAME
+#sudo cp /etc/hosts /etc/hosts.backup
+#sudo sed -i -e "s/^127.0.1.1.*$/127.0.1.1\t$HNAME/" /etc/hosts
 
 # Enable IP forwarding
 sudo sed -i -e 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
@@ -39,8 +39,8 @@ sudo iptables -A FORWARD -i $EXT -o $INT -m state --state RELATED,ESTABLISHED -j
 sudo iptables -A FORWARD -i $INT -o $EXT -j ACCEPT
 
 # Accept ssh access (not verified)
-sudo iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+#sudo iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+#sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 # Permanently save the NAT rules to a file
 sudo iptables-save | sudo tee /etc/iptables/rules.v4
@@ -53,37 +53,37 @@ sudo iptables-save | sudo tee /etc/iptables/rules.v4
 # Config network interfaces:
 # Show all network interfaces
 
-ifconfig | grep "Link"
+#ifconfig | grep "Link"
 
 # Config network interfaces:
-sudo cp /etc/network/interfaces /etc/network/interfaces.backup
-cat << CFG | sudo tee /etc/network/interfaces
-source /etc/network/interfaces.d/*
+#sudo cp /etc/network/interfaces /etc/network/interfaces.backup
+#cat << CFG | sudo tee /etc/network/interfaces
+#source /etc/network/interfaces.d/*
 
 # The loopback network interface
-auto lo
-iface lo inet loopback
+#auto lo
+#iface lo inet loopback
 
 # The internal network interface
-auto $INT
-iface $INT inet static
-address $INT_ADDR
-netmask 255.255.255.0
-broadcast 10.0.0.255
+#auto $INT
+#iface $INT inet static
+#address $INT_ADDR
+#netmask 255.255.255.0
+#broadcast 10.0.0.255
 
 # The external network interface
-auto $EXT
-iface $EXT inet dhcp
+#auto $EXT
+#iface $EXT inet dhcp
 # The host-only network interface
 #auto $HOSTONLY
 #iface $HOSTONLY inet dhcp
 
-CFG
+#CFG
 
 # Reset network service
-sudo ip addr flush $INT
-sudo ip addr flush $EXT
-sudo service networking restart
+#sudo ip addr flush $INT
+#sudo ip addr flush $EXT
+#sudo service networking restart
 
 # Review network info
-ifconfig
+#ifconfig
